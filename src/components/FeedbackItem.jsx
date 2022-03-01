56d43a004c98531d545644dba1ba4fb58b2234ce
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-28 20:48:04 -0500

    Add files via upload

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+
+
+function FeedbackItem({item}) {
+
+  return (
+    <div className="card">
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+      
+    </div>
+  );
+}
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem;
