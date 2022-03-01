96865f6fb73b887f46be4a3e8beec08cac8a3966
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-03-01 12:50:26 -0500

    Add files via upload

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,19 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
-
+import Card from './shared/Card';
 
 function FeedbackItem({item}) {
 
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => console.log(item.id)} className="close">
+      <FaTimes color='purple' />
+      </button>
+      
       <div className="text-display">{item.text}</div>
       
-    </div>
+    </Card>
   );
 }
 FeedbackItem.propTypes = {
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 
 function FeedbackList({feedback}) {
     console.log(feedback)
@@ -13,6 +14,17 @@ function FeedbackList({feedback}) {
       ))}
   </div>
   
+}
+
+FeedbackList.propTypes= {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+
+        })
+    )
 }
 
 export default FeedbackList;
