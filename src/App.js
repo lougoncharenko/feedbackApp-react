2f10ab173b68a253e2c5493af25907f691d2bec8
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-03-02 20:42:56 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header"
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App(){
  const [feedback, setFeedback]= useState(FeedbackData)
@@ -20,6 +21,7 @@ function App(){
     <>
     <Header />
     <div className='container'> 
+    <FeedbackForm />
     <FeedbackStats feedback ={feedback}/>
     <FeedbackList feedback={feedback}
     handleDelete={deleteFeedback}/>
